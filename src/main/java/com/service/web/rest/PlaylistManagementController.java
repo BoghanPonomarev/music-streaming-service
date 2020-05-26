9e16deced3464def179c1e8b2b1858ce844bf4d8
bohdan.ponomarev <bohdan.ponomarev@gmail.com>
2020-05-27 00:16:45 +0300

    Add multi audio file adding and create flyway infrustructure

--- src/main/java/com/service/web/rest/PlaylistManagementController.java
+++ src/main/java/com/service/web/rest/PlaylistManagementController.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
@@ -26,68 +28,73 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PlaylistManagementController {
 
-  private final PlaylistManagementService playlistManagementService;
-  private final StreamManagementService streamManagementService;
+    private final PlaylistManagementService playlistManagementService;
+    private final StreamManagementService streamManagementService;
 
-  @ResponseBody
-  @ResponseStatus(HttpStatus.CREATED)
-  @PostMapping(value = "/streams/{streamName}", produces = "application/json")
-  public ResponseEntity<ResourceCreationResponse<Long>> createPlaylist(@PathVariable("streamName") String streamName) {
-    Long newStreamId = streamManagementService.createStream(streamName);
-    return ResponseEntity.ok(new ResourceCreationResponse<>(newStreamId));
-  }
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/streams/{streamName}", produces = "application/json")
+    public ResponseEntity<ResourceCreationResponse<Long>> createPlaylist(@PathVariable("streamName") String streamName) {
+        Long newStreamId = streamManagementService.createStream(streamName);
+        return ResponseEntity.ok(new ResourceCreationResponse<>(newStreamId));
+    }
 
-  @ResponseBody
-  @PutMapping(value = "/streams/{streamName}/video", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResourceCreationResponse<Long>> updateVideo(@PathVariable("streamName") String streamName,
-                                                                    @RequestPart("video") MultipartFile video) throws IOException {
-    Long newVideoId = playlistManagementService.updateVideo(streamName, video.getInputStream(), video.getOriginalFilename());
-    return ResponseEntity.ok(new ResourceCreationResponse<>(newVideoId));
-  }
+    @ResponseBody
+    @PutMapping(value = "/streams/{streamName}/video", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResourceCreationResponse<Long>> updateVideo(@PathVariable("streamName") String streamName,
+                                                                      @RequestPart("video") MultipartFile video) throws IOException {
+        Long newVideoId = playlistManagementService.updateVideo(streamName, video.getInputStream(), video.getOriginalFilename());
+        return ResponseEntity.ok(new ResourceCreationResponse<>(newVideoId));
+    }
 
-  @ResponseBody
-  @PostMapping(value = "/streams/{streamName}/audio", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResourceCreationResponse<Long>> addAudio(@PathVariable("streamName") String streamName,
-                                                                 @RequestPart("audio") MultipartFile video) throws IOException {
-    Long newAudioId = playlistManagementService.addAudioFile(streamName, video.getInputStream(), video.getOriginalFilename());
-    return ResponseEntity.ok(new ResourceCreationResponse<>(newAudioId));
-  }
+    @ResponseBody
+    @PostMapping(value = "/streams/{streamName}/audio", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResourceCreationResponse<List<Long>>> addAudio(@PathVariable("streamName") String streamName,
+                                                                         @RequestPart("audios") MultipartFile[] audios) throws IOException {
+      List<Long> createdNewAudiosIds = new ArrayList<>();
+
+      for (MultipartFile audio : audios) {
+        createdNewAudiosIds.add(playlistManagementService.addAudioFile(streamName, audio.getInputStream(),
+                audio.getOriginalFilename()));
+      }
+        return ResponseEntity.ok(new ResourceCreationResponse<>(createdNewAudiosIds));
+    }
 
 
-  @PutMapping(value = "/streams/{streamName}/pr", produces = MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<String> updatePreview(@PathVariable("streamName") String streamName,
-                                                          @RequestPart("preview") MultipartFile previewFile) throws IOException {
-    playlistManagementService.updatePreview(streamName, previewFile.getInputStream());
-    return ResponseEntity.ok("OK");
-  }
+    @PutMapping(value = "/streams/{streamName}/pr", produces = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> updatePreview(@PathVariable("streamName") String streamName,
+                                                @RequestPart("preview") MultipartFile previewFile) throws IOException {
+        playlistManagementService.updatePreview(streamName, previewFile.getInputStream());
+        return ResponseEntity.ok("OK");
+    }
 
-  @PutMapping(value = "/streams/{streamName}/contentfile", produces = MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<String> getContentFile(@PathVariable("streamName") String streamName,
-                                                           @RequestPart("content") MultipartFile fullContentFile) throws IOException {
-    playlistManagementService.updateCompiledContentFile(streamName, fullContentFile.getInputStream());
-    return ResponseEntity.ok("OK");
-  }
+    @PutMapping(value = "/streams/{streamName}/contentfile", produces = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> getContentFile(@PathVariable("streamName") String streamName,
+                                                 @RequestPart("content") MultipartFile fullContentFile) throws IOException {
+        playlistManagementService.updateCompiledContentFile(streamName, fullContentFile.getInputStream());
+        return ResponseEntity.ok("OK");
+    }
 
-  @ResponseBody
-  @DeleteMapping(value = "/audios/{audioId}")
-  public ResponseEntity<String> deleteAudio(@PathVariable("audioId") Long audioId) {
-    playlistManagementService.deleteAudioFile(audioId);
-    return ResponseEntity.ok("OK");
-  }
+    @ResponseBody
+    @DeleteMapping(value = "/audios/{audioId}")
+    public ResponseEntity<String> deleteAudio(@PathVariable("audioId") Long audioId) {
+        playlistManagementService.deleteAudioFile(audioId);
+        return ResponseEntity.ok("OK");
+    }
 
-  @ResponseBody
-  @GetMapping(value = "/playlists/{streamName}", produces = "application/json")
-  public ResponseEntity<PlaylistDto> getPlaylist(@PathVariable("streamName") String streamName) {
-    return ResponseEntity.ok(playlistManagementService.getPlaylist(streamName));
-  }
+    @ResponseBody
+    @GetMapping(value = "/playlists/{streamName}", produces = "application/json")
+    public ResponseEntity<PlaylistDto> getPlaylist(@PathVariable("streamName") String streamName) {
+        return ResponseEntity.ok(playlistManagementService.getPlaylist(streamName));
+    }
 
-  @ResponseBody
-  @PutMapping(value = "/playlists/{streamName}/title", produces = "application/json")
-  public ResponseEntity<String> updateTitle(@PathVariable("streamName") String streamName,
-                                                 @RequestBody StreamTitleDto streamTitleDto) {
-    playlistManagementService.updateTitle(streamName, streamTitleDto);
-    return ResponseEntity.ok("OK");
-  }
+    @ResponseBody
+    @PutMapping(value = "/playlists/{streamName}/title", produces = "application/json")
+    public ResponseEntity<String> updateTitle(@PathVariable("streamName") String streamName,
+                                              @RequestBody StreamTitleDto streamTitleDto) {
+        playlistManagementService.updateTitle(streamName, streamTitleDto);
+        return ResponseEntity.ok("OK");
+    }
 
 }
 
---
+++ src/main/resources/db/migration/V26052020__add_test_column.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `music`.`admin_token`
+ADD COLUMN `test` VARCHAR(45) NULL AFTER `token`;
